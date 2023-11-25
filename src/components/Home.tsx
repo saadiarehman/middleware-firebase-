866e1592c52725155098df622ea655e1e6be3088
saadiarehman <saadiarehman25@gmail.com>
2023-11-25 11:17:35 +0500

    home page update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,15 @@
-import {Link} from "react-router-dom"
+
 
 function Home(props) {
   return (
-    <div>
-        <div  className="">
-            <Link to="/login" >Login</Link>
+        
+        <div className="w-screen h-screen bg-purple-400 flex justify-center items-center">
+          <div className="w-1/3 h-3/4 bg-white rounded-md flex justify-center">
+            <div className="p-10 text-2xl font-bold font-serif">
+              <p>{ props.name ? `Welcome - ${props.name}` : "Login Please"}</p>
+            </div>
+          </div>
         </div>
-        <div  className="">
-        <Link to="/signup">Signup</Link>
-        </div>
-        <div>
-            <p>{ props.name ? `Welcome - ${props.name}` : "Login Please"}
-
-</p>
-        </div>
-      
-    </div>
   )
 }
 
